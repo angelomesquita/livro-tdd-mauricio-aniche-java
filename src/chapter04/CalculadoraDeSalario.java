ed5e0b46734314c19c894b6854bccfa41f7eefab
Angelo Mesquita <angeloamesquita@gmail.com>
2023-06-07 20:00:15 -0300

    primeira versão da calculadora de salario

---
+++ src/chapter04/CalculadoraDeSalario.java
@@ -0,0 +1,13 @@
+package chapter04;
+
+
+public class CalculadoraDeSalario {
+    
+    public double calculaSalario(Funcionario funcionario) {
+        if (funcionario.getCargo().equals(Cargo.DESENVOLVEDOR)) {
+            if (funcionario.getSalario() > 3000) return 3200.0;
+            return 1350.0;
+        }
+        return 425.0;
+    }
+}
