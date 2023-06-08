268d87f8897c56b3ee730fd46bf814973f0625a4
Angelo Mesquita <angeloamesquita@gmail.com>
2023-06-07 21:19:07 -0300

    primeira versao do Carrinho de Compras

---
+++ src/chapter05/CarrinhoDeCompras.java
@@ -0,0 +1,22 @@
+package chapter05;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+public class CarrinhoDeCompras {
+    private List<Item> itens;
+
+    public CarrinhoDeCompras() {
+        this.itens = new ArrayList<>();
+    }
+    
+    public void adiciona(Item item) {
+        this.itens.add(item);
+    }
+    
+    public List<Item> getItens() {
+        return Collections.unmodifiableList(itens);
+    }
+}
