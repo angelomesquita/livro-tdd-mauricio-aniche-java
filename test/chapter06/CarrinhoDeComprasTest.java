ebdb5a57cd3867640ca22e379cfe7847a8a385b9
Angelo Mesquita <angeloamesquita@gmail.com>
2023-06-14 20:30:08 -0300

    remove duplicação da criação do objeto CarrinhoDeCompras

---
+++ test/chapter06/CarrinhoDeComprasTest.java
@@ -0,0 +1,38 @@
+package chapter06;
+
+import chapter05.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+
+public class CarrinhoDeComprasTest {
+    
+    private CarrinhoDeCompras carrinho;
+    
+    @Before
+    public void inicializa() {
+        this.carrinho = new CarrinhoDeCompras();
+    }
+    
+    @Test
+    public void deveRetornarZeroSeCarrinhoVazio() {
+        assertEquals(0.0, carrinho.maiorValor(), 0.0001);
+    }
+    
+    @Test
+    public void deveRetornarValorDoItemSeCarrinhoCom1Elemento() {
+        carrinho.adiciona(new Item("Geladeira", 1, 900.0));
+        
+        assertEquals(900.0, carrinho.maiorValor(), 0.0001);
+    }
+    
+    @Test
+    public void 
+    deveRetornarMaiorValorSeCarrinhoContemMuitosElementos() {
+        carrinho.adiciona(new Item("Geladeira", 1, 900.0));
+        carrinho.adiciona(new Item("Fogão", 1, 1500.0));
+        carrinho.adiciona(new Item("Máquina de Lavar", 1, 750.0));
+
+        assertEquals(1500.0, carrinho.maiorValor(), 0.0001);
+    }
+}
