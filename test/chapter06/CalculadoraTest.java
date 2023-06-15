d4f0f9e0e363ae4f8007dd3319fe62db8b3f2afd
Angelo Mesquita <angeloamesquita@gmail.com>
2023-06-14 21:35:59 -0300

    implementação dos testes do capítulo 06

--- test/chapter06/CalculadoraTest.java
+++ test/chapter06/CalculadoraTest.java
@@ -6,17 +6,28 @@ import static org.junit.Assert.*;
 
 public class CalculadoraTest {
     @Test
-    public void deveSomarUmMaisUm() {
-        assertEquals(2, new Calculadora().soma(1, 1));
-    }
-    
-    @Test
-    public void deveSomarUmMaisDois() {
-        assertEquals(3, new Calculadora().soma(1, 2));
-    }
-    
-    @Test
-    public void deveSomarDoisMaisDois() {
+    public void deveSomarDoisNumerosPositivos() {
         assertEquals(4, new Calculadora().soma(2, 2));
+    }
+    
+    @Test
+    public void deveSomarPositivoComNegativo() {
+        assertEquals(4, new Calculadora().soma(6, -2));
+    }
+    
+    @Test
+    public void deveSomarNegativoComPositivo() {
+        assertEquals(-4, new Calculadora().soma(-6, 2));
+    }
+    
+    @Test
+    public void deveSomarDoisNumerosNegativos() {
+        assertEquals(-4, new Calculadora().soma(-2, -2));
+    }
+    
+    @Test
+    public void deveSomarComZero() {
+        assertEquals(4, new Calculadora().soma(4, 0));
+        assertEquals(4, new Calculadora().soma(0, 4));
     }
 }
