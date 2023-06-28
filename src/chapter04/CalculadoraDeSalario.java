8f5dc897d185983e25f2a1fb7c7bda34681e93ca
Angelo Mesquita <angeloamesquita@gmail.com>
2023-06-28 20:05:30 -0300

    criando os testes para todos os cargos

--- src/chapter04/CalculadoraDeSalario.java
+++ src/chapter04/CalculadoraDeSalario.java
@@ -4,9 +4,19 @@ package chapter04;
 public class CalculadoraDeSalario {
     
     public double calculaSalario(Funcionario funcionario) {
-        if (funcionario.getSalario() > 3000) {
-            return funcionario.getSalario() * 0.8;
+        if (funcionario.getCargo().equals(Cargo.DESENVOLVEDOR)) {
+            if (funcionario.getSalario() > 3000) {
+                return funcionario.getSalario() * 0.8;
+            }
+            return funcionario.getSalario() * 0.9;
         }
-        return funcionario.getSalario() * 0.9;
+        else if (funcionario.getCargo().equals(Cargo.DBA)
+        || funcionario.getCargo().equals(Cargo.TESTADOR)) {
+            if (funcionario.getSalario() < 2500) {
+                return funcionario.getSalario() * 0.85;
+            }
+            return funcionario.getSalario() * 0.75;
+        }
+        throw new RuntimeException("Funcionário inválido");
     }
 }
