e6b84955937fe31626ef21e326e953bc4acc2d31
Angelo Mesquita <angeloamesquita@gmail.com>
2023-06-28 21:13:07 -0300

    implementações do capítulo 07

--- src/chapter04/CalculadoraDeSalario.java
+++ src/chapter04/CalculadoraDeSalario.java
@@ -4,30 +4,9 @@ package chapter04;
 public class CalculadoraDeSalario {
     
     public double calculaSalario(Funcionario funcionario) {
-        if (funcionario.getCargo().equals(Cargo.DESENVOLVEDOR)) {
-            return dezOuVintePorCentoDeDesconto(funcionario);
-        }
-        else if (funcionario.getCargo().equals(Cargo.DBA)
-        || funcionario.getCargo().equals(Cargo.TESTADOR)) {
-            return quinzeOuVinteCincoPorCentoDeDesconto(funcionario);
-            
-        }
-        throw new RuntimeException("Funcionário inválido");
-    }
-    
-    private double
-    dezOuVintePorCentoDeDesconto(Funcionario funcionario) {
-        if (funcionario.getSalario() > 3000) {
-            return funcionario.getSalario() * 0.8;
-        }
-        return funcionario.getSalario() * 0.9;
-    }
-    
-    private double
-    quinzeOuVinteCincoPorCentoDeDesconto(Funcionario funcionario) {
-        if (funcionario.getSalario() < 2500) {
-            return funcionario.getSalario() * 0.85;
-        }
-        return funcionario.getSalario() * 0.75;
+        return funcionario
+            .getCargo()
+            .getRegra()
+            .calcula(funcionario);
     }
 }
---
+++ src/chapter07/DezOuVintePorCento.java
@@ -0,0 +1,19 @@
+package chapter07;
+
+public class DezOuVintePorCento extends RegraDeCalculo {
+
+    @Override
+    protected int limite() {
+        return 3000;
+    }
+
+    @Override
+    protected double porcentagemAcimaDoLimite() {
+        return 0.8;
+    }
+
+    @Override
+    protected double porcentagemBase() {
+        return 0.9;
+    }
+}
